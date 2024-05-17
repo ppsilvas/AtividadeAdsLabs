a617fe2d7f22117a12b65eff049a50b9ebba5c65
ppsilvas <ppsilvasantana@gmail.com>
2024-05-17 19:23:00 -0300

    API concluída

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 require('dotenv').config({path: "./.env"});
 require("./database/database");
 const express = require("express");
-const pessoaRouter = require("./routers/pessoa");
+const pessoaRouter = require("./routers/responsavel");
 const tarefaRouter = require("./routers/tarefa");
 
 const app = express();
 app.use(express.json());
-app.use("/pessoa", pessoaRouter);
+app.use("/responsavel", pessoaRouter);
 app.use("/tarefa", tarefaRouter);
 
 app.listen(process.env.PORT,console.log(`Escutando na porta ${process.env.PORT}`));
